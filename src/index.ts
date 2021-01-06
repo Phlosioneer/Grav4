03557527f5a036b4237be98bf19d18372d6118c2
Intrebute <ke.seas.feliz@gmail.com>
2021-01-05 22:53:03 -0400

    Add expect function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 
 initCanvas();
 
+/**
+ * Returns the given value `t` and asserts it is non-null.
+ * If it is in fact null, throws `e` as an exception.
+ * 
+ * Might act strangely if `T` contains valid but falsey values.
+ */
+function expect<T>(t: T | null, e: any): T {
+    if (!t) {
+        throw e;
+    } else {
+        return t;
+    }
+}
+
 function initCanvas() {
     const canvasHtml = document.getElementById("canvas");
     if (!canvasHtml) {
